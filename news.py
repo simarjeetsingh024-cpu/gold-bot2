4a72b5b188cebf593d177b42173eacc372d6c8c1
simarjeetsingh024-cpu <simarjeetsingh024@gmail.com>
2025-11-21 21:46:05 +0000

    Create is_news_blocked function in news.py
    Added a placeholder function for news filtering.

---
+++ news.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+
+def is_news_blocked(now: datetime) -> bool:
+    """
+    Placeholder news filter.
+    Later we will integrate:
+    - FOMC
+    - CPI
+    - PPI
+    - NFP
+    - Red USD events
+    """
+    return False
